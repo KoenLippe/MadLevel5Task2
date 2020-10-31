c85ad9f9cc81ddc2bf511d617ed01f9df3b667c3
Koen Lippe <koen.lippe@quicknet.nl>
2020-10-31 16:47:45 +0100

    Setup navigation between fragments

--- app/src/main/java/com/example/madlevel5task2/AddGameFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/AddGameFragment.kt
@@ -5,27 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_game.*
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class AddGameFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_add_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        fabAddGame.setOnClickListener {
+            findNavController().navigate(R.id.action_AddGameFragment_to_OverviewFragment)
         }
     }
 }
